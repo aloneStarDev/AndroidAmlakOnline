bed1203c6c518d357aca275ac768820d6aa5614f
star <am13781206@gmail.com>
2020-05-30 03:02:08 +0430

    it seem that its my last Start

--- app/src/main/java/com/star/amlakonline/MainActivity.java
+++ app/src/main/java/com/star/amlakonline/MainActivity.java
@@ -1,14 +1,65 @@
 package com.star.amlakonline;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle adbr;
+    private ImageButton menuBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        dl = (DrawerLayout)findViewById(R.id.drawer_layout);
+        menuBtn = (ImageButton) findViewById(R.id.toolBarMenuBtn);
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("RtlHardcoded")
+            @Override
+            public void onClick(View v) {
+                dl.openDrawer(Gravity.RIGHT,true);
+            }
+        });
+        adbr = new ActionBarDrawerToggle(this,dl,R.string.NavbarOpen,R.string.NavbarClose);
+        adbr.setDrawerIndicatorEnabled(true);
+        dl.addDrawerListener(adbr);
+        adbr.syncState();
+        setSupportActionBar(toolbar);
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    int id = item.getItemId();
+                    switch(id)
+                    {
+                        case R.id.nav_login:
+                            Toast.makeText(MainActivity.this, "login",Toast.LENGTH_SHORT).show();break;
+                        case R.id.nav_register:
+                            Toast.makeText(MainActivity.this, "register",Toast.LENGTH_SHORT).show();break;
+                        default:
+                            return true;
+                    }
+                    return true;
+                }
+        });
+
+
     }
+
 }
